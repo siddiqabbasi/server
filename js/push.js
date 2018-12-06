b29bfa6fadf34704e1a343a945e0ed8dccded32b
Siddiq Abbasi <siddiqabbasi0@gmail.com>
2018-12-06 19:06:28 +0500

    changes

--- js/push.js
+++ js/push.js
@@ -2,7 +2,7 @@ const debug = require('debug')('push:notifications');
 
 const settings = {
   gcm: {
-    id: 'AAAA9U3NkO8:APA91bE9XnPD1WQGYle0dbJs6KO6HmAq_73f_z2PHoPr3sbRovXbn9q-PyJAKYlxgNI88rBpTzNWCXRuxhX9FcwbDBhT4h0BAiJr4BiEx8uSkWHXM92q04Q6cydK-dHGSnR20bsgH0Lg',
+    id: 'AAAAXFegkME:APA91bFDBjdsB02tlKvi6-e68RTULRHyOzEr_bGd68GCSMXoqQmIPtEXCmNHcK_2aTipoXKjI5rWWhxRKPTtNwy7lBkMfkKAsS5Fxuvjd4h2pXlT4IRPeTgvmb-spKzlkZT_GJVZlSfI',
   },
   apn: {
     token: {
@@ -13,7 +13,8 @@ const settings = {
   },
 };
 console.log(settings, 'settingsssssss')
-
+// AAAAXFegkME:APA91bFDBjdsB02tlKvi6-e68RTULRHyOzEr_bGd68GCSMXoqQmIPtEXCmNHcK_2aTipoXKjI5rWWhxRKPTtNwy7lBkMfkKAsS5Fxuvjd4h2pXlT4IRPeTgvmb-spKzlkZT_GJVZlSfI
+// AAAA9U3NkO8:APA91bE9XnPD1WQGYle0dbJs6KO6HmAq_73f_z2PHoPr3sbRovXbn9q-PyJAKYlxgNI88rBpTzNWCXRuxhX9FcwbDBhT4h0BAiJr4BiEx8uSkWHXM92q04Q6cydK-dHGSnR20bsgH0Lg
 const PushNotifications = require('node-pushnotifications');
 
 const push = new PushNotifications(settings);
